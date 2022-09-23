b14100314b459bfbf9e96fa348ce60efd59cd933
PolySim <simon.desdevises@gmail.com>
2022-09-23 14:09:59 +0200

    responsive

--- src/container/Game/index.tsx
+++ src/container/Game/index.tsx
@@ -2,12 +2,24 @@ import React, { useState, useEffect } from "react";
 import { Game } from "src/styled";
 
 export default function GameView(): JSX.Element {
+  const [width, setWidth] = useState<number>(window.innerWidth);
+  const [height, setHeight] = useState<number>(window.innerHeight);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWidth(window.innerWidth);
+      setHeight(window.innerHeight);
+    };
+    window.addEventListener("resize", handleResize);
+  }, []);
+
   return (
     <Game
-      style={{
-        width: "500px",
-        height: "500px",
-      }}
+      style={
+        width / 1.3 > 0.8 * height
+          ? { width: 0.8 * height, height: 0.8 * height }
+          : { width: width / 1.3, height: width / 1.3 }
+      }
     ></Game>
   );
 }
--- src/styled.ts
+++ src/styled.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const Game = styled.div`
   position: absolute;
-  top: 50%;
+  top: 55%;
   left: 50%;
   transform: translate(-50%, -50%);
   border-radius: 20px;
@@ -11,8 +11,9 @@ export const Game = styled.div`
 
 export const Title = styled.h1`
   text-align: center;
+  width: 100%;
   color: #fe4a49;
-  font-size: 8vw;
-  margin-top: 2.5%;
+  font-size: min(4vw, 70px);
+  padding-top: 1%;
   font-family: "Courier New", Courier, monospace;
 `;
